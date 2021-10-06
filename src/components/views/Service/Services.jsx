59969438a9d5bba7849b1e631ee58c1aa4790627
Enzo Santilli <enzo135246@gmail.com>
2021-10-06 08:54:05 -0300

    Terminado casi todos los copy de los servicios. Falta Estrategia y gestión de publicidad.

--- src/components/views/Service/Services.jsx
+++ src/components/views/Service/Services.jsx
@@ -38,7 +38,7 @@ const ModalService = ({description, image, title, close})=>{
                 </div>
                 <div className="modalService-text">
                     <h5>{title}</h5>
-                    <p>{description}</p>
+                    <div>{description}</div>
                     <LinkScroll to="contact" className="callToActionBtn-container" onClick={handleClose}>
                         ¿Te quedan dudas? Escribinos!
                     </LinkScroll>
@@ -80,13 +80,18 @@ const Services = () => {
     const content= [
         {
             title: "Social Media Plan",
-            description:<p key="p1">Ofrecemos estrategias integrales para el desarrollo y cumplimiento de tus objetivos de Marketing en Facebook, Instagram, Youtube, y más plataformas sociales a través de estrategias integradas a la web que apuntan a maximizar el valor de las RR.SS para tu negocio.</p>,
+            description:<p key="p1">Ofrecemos un estudio de tu empresa. A partir de los datos obtenidos estableceremos objetivos, analizaremos los resultados, y podremos compararlos con los de la competencia. De esta forma definiremos las estrategias para mejorar el rendimiento de las redes sociales.</p>,
             img: socialMedia,
             itemClass: "",
         },
         {
             title: "Community manager",
-            description:<p key="p1">Te alojamos tu página web en los mejores y más confiables servidores.</p>,
+            description:[<p key="p1">Las principales funciones de un Community Manager (cm) son las siguientes: </p>,
+                        <ul key="p2">
+                            <li>Analizar la competencia:  una de las utilidades del community manager es advertir qué contenido debe ser evitado para no ser vistos como imitadores de otras marcas.</li>
+                            <li>Generar contenido: El CM es el encargado de publicar y (a veces) redactar los contenidos del blog, para lo que necesita tener claro cuál es el momento indicado y cómo redactar el título más atractivo, sin caer en sensacionalimos o amarillismo.</li>
+                            <li>Monitorear constantemente: Durante el día estaremos monitoreando, respondiendo consultas o buscando conversar con los usuarios sea porque nos mencionan directamente o porque usan alguna palabra clave que está directamente relacionada con nuestro servicio.</li>
+                        </ul>],
             img: cm,
             itemClass: "",
         },
@@ -100,9 +105,9 @@ const Services = () => {
         {
             title: "Diseño web",
             description:[<p key="p1">Hoy en día es vital para una empresa tener su propio sitio web, ya que brinda credibilidad ante los clientes potenciales y ayuda a ser visible en los buscadores.</p>,
-                        <p>Nosotros te ofrecemos la posibilidad de que puedas tener tu propia web! Aplicamos diseños únicos para que tu página se destaque entre tus competidores ayudandote a crecer en ventas e impactar positivamente con tus clientes.</p>,
-                        <p>Para que tu experiencia sea la mejor te ofrecemos lo siguiente: </p>,
-                        <ul>
+                        <p key="p2">Nosotros te ofrecemos la posibilidad de que puedas tener tu propia web! Aplicamos diseños únicos para que tu página se destaque entre tus competidores ayudandote a crecer en ventas e impactar positivamente con tus clientes.</p>,
+                        <p key="p3">Para que tu experiencia sea la mejor te ofrecemos lo siguiente: </p>,
+                        <ul key="p4">
                             <li>Diseño y desarrollo web</li>
                             <li>Mantenimiento</li>
                             <li>Hosting</li>
