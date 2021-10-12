fe1a23831d778f2939944d32cce483c41feb9755
Enzo Santilli <enzo135246@gmail.com>
2021-10-12 12:19:01 -0300

    Actualización social media

--- src/components/views/Contact/SocialMedia/SocialMedia.jsx
+++ src/components/views/Contact/SocialMedia/SocialMedia.jsx
@@ -6,10 +6,10 @@ const SocialMedia = () => {
         <div className="socialMedia-container">
             <div className="redes_sociales">
                 <ul>
-                    <li className="wow animate__animated animate__backInRight" data-wow-delay="0.6s"><a href="https://wa.me/542634786580"><i className="fab fa-whatsapp"></i></a></li>
-                    <li className="wow animate__animated animate__backInRight" data-wow-delay="0.8s"><a href="https://www.facebook.com/spaceappsdev"><i className="fab fa-facebook-f"></i></a></li>
-                    <li className="wow animate__animated animate__backInRight" data-wow-delay="1s"><a href="https://www.instagram.com/spaceappsdev/?hl=es-la"><i className="fab fa-instagram"></i></a></li>
-                    <li className="wow animate__animated animate__backInRight" data-wow-delay="1.2s"><a href="https://twitter.com/spaceappsdev"><i className="fab fa-twitter"></i></a></li>
+                    <li className="wow animate__animated animate__backInRight" data-wow-delay="0.6s"><a href="https://wa.me/542634786580" target="_blank" rel="noreferrer"><i className="fab fa-whatsapp"></i></a></li>
+                    <li className="wow animate__animated animate__backInRight" data-wow-delay="0.8s"><a href="https://www.facebook.com/spaceappsdev" target="_blank" rel="noreferrer"><i className="fab fa-facebook-f"></i></a></li>
+                    <li className="wow animate__animated animate__backInRight" data-wow-delay="1s"><a href="https://www.instagram.com/spaceappsdev/?hl=es-la" target="_blank" rel="noreferrer"><i className="fab fa-instagram"></i></a></li>
+                    <li className="wow animate__animated animate__backInRight" data-wow-delay="1.2s"><a href="https://www.linkedin.com/company/space-apps-dev/" target="_blank" rel="noreferrer"><i className="fab fa-linkedin-in"></i></a></li>
                 </ul>
             </div>
         </div>
